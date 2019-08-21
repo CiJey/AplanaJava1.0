b50825eaa21893b54b2235d35c63eeaf627f38e5
Sergey Gasparyan <sergey_aaa@mail.ru>
2019-08-21 11:27:34 +0300

    HW8

---
+++ Work8/src/CycleOutput.java
@@ -0,0 +1,10 @@
+/**
+ * Необходимо с помощью цикла вывести на экран числа от 1 до 50.
+ */
+public class CycleOutput {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++)
+            System.out.println(i);
+    }
+}
